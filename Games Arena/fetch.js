cee8083e726a4d44a9a23891f001881f5d4af06c
aryan21710 <aryan21710@gmail.com>
2019-02-05 12:48:22 +0530

    COMMITTED SEARCH BY TITLE CODE

--- Games Arena/fetch.js
+++ Games Arena/fetch.js
@@ -77,8 +77,117 @@ class GamesConfig {
         document.getElementById('first').innerHTML='';
         document.getElementById('first').disabled=true;
     }  
-    return pageCount;
+      return pageCount;
     }
+
+
+
+    searchGame(e) {
+        console.log('INSIDE SEARCH GAME NOW:-');
+        if (this.gameList.length == 99) {
+            e.preventDefault();
+            console.log('USER INTERED:-' + e.target.value);
+            const userInput=e.target.value.toLowerCase();
+            let gameFound=[];
+            this.gameList.forEach((v)=>{
+                if (v['title'].toLowerCase().includes(userInput)) {
+                    console.log('found:-'+v);
+                    gameFound.push(v);
+                }
+            })
+            console.log('gameFound Details are:-' + JSON.stringify(gameFound));
+            return gameFound;
+        } else {
+          alert('DATA NOT READY YET.. TRY AGAIN');
+        }
+
+    }
+
+
+
+    showFoundGame(gameFound) {
+        console.log('INSIDE SHOWFOUNDGAME:-' + gameFound.length);
+        document.querySelector('#gameContainer').innerHTML='';
+        let output='';
+        gameFound.forEach((g, ind) => {
+            output+= `<div id="game" class="g1"> <p id="title">Title:- ${g['title']}</p> <p id="platform">Platform:- ${g['platform']}</p> <p id="score">Score:- ${g['score']}</p> <p id="genre">Genre:- ${g['genre']}</p> <p id="editorsChoice">Editors_Choice:- ${g['editors_choice']}</p></div >`
+        })
+        document.querySelector('#gameContainer').innerHTML=output;
+
+    }
+
+
+    initialState(gameContainerData) {
+        console.log('CALLING INTIAL STATE NOW');
+        // console.log(gameContainerData);
+        // document.getElementById('gameContainer').innerHTML = gameContainerData;
+        document.getElementById('gameContainer').innerHTML=`<div id="game" class="g1">
+                            <p id="title">"TITLE": "LittleBigPlanet PS Vita",</p>
+                            <p id="platform">"Platform": "PlayStation Vita",</p>
+                            <p id="score">"Score": 9,</p>
+                            <p id="genre">"Genre": "Platformer",</p>
+                            <p id="editorsChoice">"Editors_choice": "Y"</p>
+                    </div>
+                    <div id="game" class="g2">
+                        <p id="title">"TITLE": "LittleBigPlanet PS Vita -- Marvel Super Hero Edition",</p>
+                        <p id="platform">"Platform": "PlayStation Vita",</p>
+                        <p id="score">"Score": 9,</p>
+                        <p id="genre">"Genre": "Platformer",</p>
+                        <p id="editorsChoice">"Editors_choice": "Y"</p>
+                    </div>
+          
+                    <div id="game" class="g3">
+                        <p id="title">"TITLE": "Splice: Tree of Life",</p>
+                            <p id="platform">"Platform": "iPad",</p>
+                            <p id="score">"Score": 8.5,</p>
+                            <p id="genre">"Genre": "Puzzle",</p>
+                            <p id="editorsChoice">"Editors_choice": "N"</p>
+                    </div>
+
+                    <div id="game" class="g4">
+                                <p id="title">"TITLE": "NHL 13",</p>
+                                <p id="platform">"Platform": "Xbox 360",</p>
+                                <p id="score">"Score": 8.5,</p>
+                                <p id="genre">"Genre": "Sports",</p>
+                                <p id="editorsChoice">"Editors_choice": "N"</p>
+                   </div>
+                   <div id="game" class="g5">
+                                <p id="title">"TITLE": "NHL 13",</p>
+                                <p id="platform">"Platform": "PlayStation 3",</p>
+                                <p id="score">"Score": 8.5,</p>
+                                <p id="genre">"Genre": "Sports",</p>
+                                <p id="editorsChoice">"Editors_choice": "N"</p>
+                   </div>
+                    <div id="game" class="g6">
+                        <p id="title">"TITLE": "Total War Battles: Shogun",</p>
+                        <p id="platform">"Platform": "Macintosh"",</p>
+                        <p id="score">"Score": 7,</p>
+                        <p id="genre">"Genre": "Strategy",</p>
+                        <p id="editorsChoice">"Editors_choice": "N"</p>
+                    </div>
+                    <div id="game" class="g7">
+                        <p id="title">"TITLE": "Double Dragon: Neon",</p>
+                        <p id="platform">"Platform": "Xbox 360",</p>
+                        <p id="score">"Score": 3,</p>
+                        <p id="genre">"Genre": "Fighting",</p>
+                        <p id="editorsChoice">"Editors_choice": "N"</p>
+                    </div>
+                    <div id="game" class="g8">
+                        <p id="title">"TITLE": "Guild Wars 2",</p>
+                        <p id="platform">"Platform": "PC",</p>
+                        <p id="score">"Score": 9,</p>
+                        <p id="genre">"Genre": "RPG",</p>
+                        <p id="editorsChoice">"Editors_choice": "Y"</p>
+                    </div>
+                    <div id="game" class="g9">
+                                <p id="title">"TITLE": "Double Dragon: Neon",</p>
+                                <p id="platform">"Platform": "PlayStation 3",</p>
+                                <p id="score">"Score": 3,</p>
+                                <p id="genre">"Genre": "Fighting",</p>
+                                <p id="editorsChoice">"Editors_choice": "N"</p>
+                   </div>`
+    }
+
 
 
 
@@ -106,8 +215,17 @@ class GamesConfig {
                     document.getElementById('first').disabled=false;
                     pageCount=this.moveToNextPage(pageCount,e);
             })
-            document.querySelector("#search").addEventListener('click',(e)=>{
-                this.searchGame();
+            document.querySelector("#search").addEventListener('input',(e)=>{
+                if (e.target.value.length > 0) {
+                    document.getElementById('nextPage').style.display = 'none';
+                    let gameFound = this.searchGame(e);
+                    this.showFoundGame(gameFound);
+                } else {
+                    this.initialState();
+                    document.getElementById('nextPage').style.display='';
+                }
+
+
             })
         
         }) 
@@ -116,18 +234,7 @@ class GamesConfig {
 
 
     
-
-    searchGame() {
-        console.log('INSIDE SEARCH GAME NOW:-');
-        try {
-            if(this.gameList.length==99) {
-                alert(this.gameList.length);
-            }
-        } catch(err) {
-            alert('GAMES DATA NOT LOADED YET.. TRY AGAIN IN FEW SECONDS')
-        }   
-    }
- }
+}
 
 
 const myUrl='http://starlord.hackerearth.com/gamesarena';
